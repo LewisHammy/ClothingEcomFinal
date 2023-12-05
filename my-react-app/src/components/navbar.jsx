fc88af9bd18f9c02937884ef98028427c971ebd6
LewisHammy <samkabir30@gmail.com>
2023-12-04 19:24:53 -0800

    added shopping cart icon using phosphor

--- my-react-app/src/components/navbar.jsx
+++ my-react-app/src/components/navbar.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ShoppingCart } from 'phosphor-react';
 
 export const Navbar = () => {
-  return <div>N</div> 
-}
+  return <div className="navbar">
+    <div className="links">
+        <Link to="/">SHOP </Link>
+        <Link to="/cart">
+            <ShoppingCart size={30} />      
+        </Link>
+    </div>
+  </div> 
+};
