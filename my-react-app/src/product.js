90fd30150c57d8a3fe7f066d0f9d61c6b4f4b63f
LewisHammy <samkabir30@gmail.com>
2023-12-04 20:21:04 -0800

    added product js file to add products and title displays well

---
+++ my-react-app/src/product.js
@@ -0,0 +1,10 @@
+import CAT from './assets/freshouttauci.png';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        ProductName: "Coding Cat",
+        price: 35,
+        productImage: CAT
+    },
+]
