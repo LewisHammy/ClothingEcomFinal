98ca44a13dbe6d0d0c36888894ee94db225c41d4
LewisHammy <samkabir30@gmail.com>
2023-12-07 21:21:02 -0800

    added more images to display, also changed the capital to uppercase in the sign up section in the navbar

--- client/src/products.js
+++ client/src/products.js
@@ -1,4 +1,7 @@
 import CAT from './assets/products/freshouttauci.png';
+import CODECAT from './assets/products/hackercat.png';
+import SIMPLE from './assets/products/keepitsimple.png';
+import LEARN from './assets/products/foreverlearning.png';
 
 export const PRODUCTS = [
     {
@@ -7,5 +10,24 @@ export const PRODUCTS = [
         price: 35,
         productImage: CAT
     },
+    {
+        id: 2,
+        ProductName: "Actual Cat Coding",
+        price: 35,
+        productImage: CODECAT
+    },
+    {
+        id: 3,
+        ProductName: "simple",
+        price: 35,
+        productImage: SIMPLE
+    },
+    {
+        id: 4,
+        ProductName: "learning",
+        price: 35,
+        productImage: LEARN
+    },
 ];
+
 
