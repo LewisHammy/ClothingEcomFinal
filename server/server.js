69fe3d588f1747798a029551e1c285ac6ee94387
physixkz <130267624+physixkz@users.noreply.github.com>
2023-12-10 20:36:50 -0800

    working graphql and insomnia routes

--- server/server.js
+++ server/server.js
@@ -4,8 +4,10 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const productRoutes = require('./routes/api/product-routes'); // Adjust the path accordingly
 
-const PORT = process.env.PORT || 5000; // Adjust the port number as needed
+
+const PORT = process.env.PORT || 5000;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -16,6 +18,14 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get('/hello', (req, res) => {
+  res.send('Hello, this is a custom route!');
+});
+
+// Mount the product routes
+app.use('/api/products', productRoutes);
+
+// Start the Apollo Server
 server.start().then(() => {
   app.use('/graphql', expressMiddleware(server));
 
