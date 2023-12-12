36b3a69b68a4a941e85ed9f91389ae6ab0ba39fa
physixkz <130267624+physixkz@users.noreply.github.com>
2023-12-11 17:21:24 -0800

    updated user.js in server, connected frontend to backend to receive product details from mongodb cloud database

--- server/models/User.js
+++ server/models/User.js
@@ -1,23 +1,61 @@
-const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const Product = require('./Product'); // Import the Product model
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    // Define savedProducts as an array of references to the Product model
+    savedProducts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+      },
+    ],
+    // Other user-related fields as needed for your application
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  // Additional fields for user profile (e.g., address, orders history, etc.)
+  {
+    // Set additional options if required
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// Hash user password before saving
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
 });
 
-const User = mongoose.model('User', userSchema);
+// Method to compare and validate password for login
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+// Virtual field to get the count of saved products
+userSchema.virtual('productCount').get(function () {
+  return this.savedProducts.length;
+});
+
+const User = model('User', userSchema);
 
 module.exports = User;
