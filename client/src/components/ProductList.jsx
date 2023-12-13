ba2d49e6ae140a5f778d52f479cc948ce5122046
Matthew Chappelle <136776175+MatthewChappelle@users.noreply.github.com>
2023-12-12 23:03:20 -0600

    Update ProductList.jsx
    fixed

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';n 
+import React, { useState, useEffect } from 'react';
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
